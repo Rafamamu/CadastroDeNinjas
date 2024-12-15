5feea7d68974c4118eb31498ce8de3737ab55faa
Rafael <rafaroga46@gmail.com>
2024-12-15 19:14:19 -0300

    feat: Criada MissoesService que injeta as dependências da interface MissoesRepository, e Criado um método listarMissoes

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissoesService.java
@@ -0,0 +1,24 @@
+package dev.java10x.CadastroDeNinjas.Missoes;
+
+import dev.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    // Listar todas as Missoes
+    public List<MissoesModel> listarMissoes() {
+        return missoesRepository.findAll();
+    }
+
+
+
+}

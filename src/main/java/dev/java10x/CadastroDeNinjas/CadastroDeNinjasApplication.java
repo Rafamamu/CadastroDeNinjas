828036abf13f89c054fbfd6379248f8c76bca538
Rafael <rafaroga46@gmail.com>
2024-12-16 23:58:58 -0300

    fix: Insere um sout com a mensagem de Executado com sucesso para melhor legibilidade na hora de iniciar a aplicação

--- src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -3,11 +3,14 @@ package dev.java10x.CadastroDeNinjas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
+		System.out.println("Executado com Sucesso...");
 	}
 
 }

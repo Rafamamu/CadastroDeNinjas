e6fec72ea58c7b3a5f0e330daba0748002c437a4
Rafael <rafaroga46@gmail.com>
2024-12-28 15:40:58 -0300

    feature: Criada WebConfig para integrar minha API rest cadastro-de-ninjas

---
+++ src/main/java/dev/java10x/CadastroDeNinjas/WebConfig.java
@@ -0,0 +1,19 @@
+package dev.java10x.CadastroDeNinjas; // Ajuste o pacote de acordo com o nome do seu projeto
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}

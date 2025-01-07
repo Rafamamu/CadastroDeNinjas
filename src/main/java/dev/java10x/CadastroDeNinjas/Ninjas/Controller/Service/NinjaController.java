c5fbc6f93c0be07c43c231bddc7d3f86e8cfa332
Rafael <rafaroga46@gmail.com>
2025-01-06 23:09:11 -0300

    fix: Pequena indentação no codigo do NinjaController

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaController.java
@@ -30,24 +30,24 @@ public class NinjaController {
 
     // Adicionar ninja (CREATE)
     @PostMapping("/criar")
-    @Operation(summary = "Cria um novo ninja",description = " Rota para Criar um novo ninja na tabela Cadastro")
+    @Operation(summary = "Cria um novo ninja", description = " Rota para Criar um novo ninja na tabela Cadastro")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "201",description = "Novo ninja criado com sucesso"),
-            @ApiResponse(responseCode = "400",description = "Erro ao criar o ninja")
+            @ApiResponse(responseCode = "201", description = "Novo ninja criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro ao criar o ninja")
     })
     public ResponseEntity<String> criarNinja(@RequestBody NinjaDTO ninja) {
         NinjaDTO novoNinja = ninjaService.criarNinja(ninja);
         return ResponseEntity.status(HttpStatus.CREATED)
-                .body("Ninja criado com sucesso: "+novoNinja.getNome()+ "(ID): "+novoNinja.getId());
+                .body("Ninja criado com sucesso: " + novoNinja.getNome() + "(ID): " + novoNinja.getId());
     }
 
 
     // Mostrar todos os Ninjas (READ)
     @GetMapping("/listar")
-    @Operation(summary = "Lista todos os ninjas",description = " Rota Lista todos os ninjas sa tabela cadastro")
+    @Operation(summary = "Lista todos os ninjas", description = " Rota Lista todos os ninjas sa tabela cadastro")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",description = "Ninja encontrado com sucesso"),
-            @ApiResponse(responseCode = "404",description = "Ninja não encontrado")
+            @ApiResponse(responseCode = "200", description = "Ninja encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Ninja não encontrado")
     })
     public ResponseEntity<List<NinjaDTO>> listarNinjas() {
         List<NinjaDTO> ninjas = ninjaService.listarNinjas();
@@ -63,32 +63,32 @@ public class NinjaController {
         if (ninja != null) {
             return ResponseEntity.ok(ninja);
 
-        }else {
+        } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("Ninja com (ID): "+ id +", não existe nos nossos registros. ");
+                    .body("Ninja com (ID): " + id + ", não existe nos nossos registros. ");
         }
     }
 
     // Alterar dados dos ninjas (UPDATE)
     @PutMapping("/alterar/{id}")
-    @Operation(summary = "Altera Ninja por id",description = "Rota para alterar ninja por id")
+    @Operation(summary = "Altera Ninja por id", description = "Rota para alterar ninja por id")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200",description = "Ninja alterado com sucesso"),
-            @ApiResponse(responseCode = "404",description = "Não foi possivel alterar o ninja")
+            @ApiResponse(responseCode = "200", description = "Ninja alterado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Não foi possivel alterar o ninja")
     })
     public ResponseEntity<?> atualizarNinja(
             @Parameter(description = "Usuário manda id no caminho da requisição")
             @PathVariable Long id,
             @Parameter(description = "Usuário Manda o corpo atualizado como caminho da requisição")
-            @RequestBody NinjaDTO ninjaAtualizado){
-        NinjaDTO ninja = ninjaService.atualizarNinja(id,ninjaAtualizado);
+            @RequestBody NinjaDTO ninjaAtualizado) {
+        NinjaDTO ninja = ninjaService.atualizarNinja(id, ninjaAtualizado);
 
         if (ninja != null) {
             return ResponseEntity.ok(ninja);
 
-        }else {
-            return  ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("O ninja com (ID): "+ id +", não foi encontrado!" );
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("O ninja com (ID): " + id + ", não foi encontrado!");
         }
     }
 
@@ -100,10 +100,10 @@ public class NinjaController {
 
         if (ninjaExistente != null) {
             ninjaService.deletarNinjaPorId(id);
-            return ResponseEntity.ok("Ninja com Id: "+id +" deletado com sucesso!");
-        }else {
+            return ResponseEntity.ok("Ninja com Id: " + id + " deletado com sucesso!");
+        } else {
             return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                    .body("O ninja com ID: "+ id +" não foi encontrado!");
+                    .body("O ninja com ID: " + id + " não foi encontrado!");
         }
     }
 }
